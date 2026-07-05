72dbbe6cedbea10b40795a18351214630bb863d7
bloodl <bloodl6062@gmail.com>
2026-07-05 15:03:07 -0400

    configurar static_root para produccion

--- bark_project/settings.py
+++ bark_project/settings.py
@@ -116,7 +116,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'core', 'static'),
+]
+
 
 # Media files (user-uploaded documents)
 MEDIA_URL = '/media/'
